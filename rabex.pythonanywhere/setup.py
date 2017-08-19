5b297f89b6f5a0197fdb8e5ca314ffa0bfcc8dfa
ragus@lenovo <zycnth@qq.com>
2017-08-19 18:26:56 +0800

    始终不明白setuptools的用法，还得研究

---
+++ rabex.pythonanywhere/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rabex.pythonanywhere.com',
+    packages= find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+    setup_requires=[
+
+    ],
+    test_requires=[
+
+    ],
+)
